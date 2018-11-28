b6cc70e81786ee2d8ffcc8cc4900463a4603b75d
s_hasegawa <s_hasegawa@jbat.co.jp>
2018-11-28 16:54:57 +0900

    refactor: サーバソースをwahhoi-apiディレクトリに移動

--- wahhoi-api/src/main/java/com/example/wahhoi/controllers/WahhoiContoller.java
+++ wahhoi-api/src/main/java/com/example/wahhoi/controllers/WahhoiContoller.java
@@ -2,6 +2,7 @@ package com.example.wahhoi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class WahhoiContoller {
 
     @GetMapping("{name}")
     public String getWahhoiString(@PathVariable("name") String name) {
-        return "\"" + name + "\" will be WAHHOI!";
+        String n = name.substring(0, name.length() >= 20 ? 20 : name.length());
+        return "\"" + n + "\" will be WAHHOI!";
     }
 }
